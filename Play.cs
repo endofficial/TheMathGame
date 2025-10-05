dca615a8d37344172239066e373538c280d19f13
endofficial <andreaverdirrame11@gmail.com>
2025-10-05 09:26:27 +0200

    #add switch statements

--- Play.cs
+++ Play.cs
@@ -2,7 +2,41 @@
 {
     public void PlayGame()
     {
-        WriteLine("Game started!");
+        Write("Enter your name: ");
+        string? playerName = ReadLine();
+        WriteLine($"Welcome to the Mathgame, {playerName}!\n");
+
+        Write("Choose an operator for the game between: \nA - '+' \nS - '-' \nM - 'x' \nD - ':' \ne - exit game");
+        string? Input_game = ReadLine();
+        switch (Input_game)
+        {
+            case "A":
+                WriteLine("You chose Addition!");
+                Addition addition = new Addition();
+                addition.StartAdditionGame(playerName);
+                break;
+            case "S":
+                WriteLine("You chose Subtraction!");
+                Subtraction subtraction = new Subtraction();
+                subtraction.StartSubtractionGame(playerName);
+                break;
+            case "M":
+                WriteLine("You chose Multiplication!");
+                Multiplication multiplication = new Multiplication();
+                multiplication.StartMultiplicationGame(playerName);
+                break;
+            case "D":
+                WriteLine("You chose Division!");
+                Division division = new Division();
+                division.StartDivisionGame(playerName);
+                break;
+            case "e":
+                WriteLine("Exiting the game. Goodbye!");
+                break;
+            default:
+                WriteLine("Invalid choice. Please select a valid operator.");
+                break;
+        }
     }
 
 }
