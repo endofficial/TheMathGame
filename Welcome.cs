5d221ffed44f0dd92e3ee244a9a968a6b26ee962
endofficial <andreaverdirrame11@gmail.com>
2025-10-06 13:38:31 +0200

    #

--- Welcome.cs
+++ Welcome.cs
@@ -4,5 +4,22 @@
     {
         WriteLine("Welcome to Mathgame!");
         WriteLine("P - PLAY \nC - CHRONOLOGY \nQ - QUIT\n");
+
+        string? input_1 = ReadLine();
+        switch (input_1)
+        {
+            case "P" or "p":
+                Play gameStart = new Play();
+                gameStart.PlayGame();
+                break;
+            case "C" or "c":
+                Chronology chronology = new Chronology();
+                chronology.StartChronology();
+                break;
+            case "Q" or "q":
+                WriteLine("You chose to quit the game, goodbye!");
+                break;
+        }
+        ;
     }
 }
