e914de5200f727325874fcd978a2d597eb8bf10a
endofficial <andreaverdirrame11@gmail.com>
2025-10-06 13:37:54 +0200

    #instance of StartWelcome

--- Program.cs
+++ Program.cs
@@ -1,18 +1,2 @@
 ﻿Welcome welcome = new Welcome();
 welcome.StartWelcome();
-
-string? input_1 = ReadLine();
-switch (input_1)
-{
-    case "P":
-        Play gameStart = new Play();
-        gameStart.PlayGame();
-        break;
-    case "C":
-        Chronology chronology = new Chronology();
-        chronology.StartChronology();
-        break; 
-    case "Q":
-        WriteLine("You chose to quit the game, goodbye!");
-        break;
-};
