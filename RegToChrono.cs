f7fe21640b158638e08c200e1c593b689f569268
endofficial <andreaverdirrame11@gmail.com>
2025-10-15 07:16:58 +0200

    #

--- RegToChrono.cs
+++ RegToChrono.cs
@@ -1,13 +1,25 @@
-﻿public class RegToChrono
+﻿public class RegToChrono 
 {
-    protected List<DateTimeOffset> _games = new List<DateTimeOffset>();
+    protected List<string> _games = new List<string>();
 
-    public DateTimeOffset GetChrono()
+    public void AddGame(string game_Time)
     {
+        _games.Add(game_Time);
+    }
+
+    public void GetChrono() //void perché non ritorna nulla, deve solo stampare
+    {
+        WriteLine("--- Games chronology ---\n");
+        if (_games.Count == 0)
+        {
+            WriteLine("\nNo games recorded!\n");
+            return;
+        }
+
         foreach (var game in _games)
         {
             WriteLine(game);
         }
-        return DateTimeOffset.Now;
+        WriteLine("\n--- End of chronology ---\n");
     }
 }
