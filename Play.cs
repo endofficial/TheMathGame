0a44f24ad69c05ec51d3e4bfdf179df848a778f4
endofficial <andreaverdirrame11@gmail.com>
2025-10-15 23:38:13 +0200

    #I added try-catch block

--- Play.cs
+++ Play.cs
@@ -8,41 +8,52 @@
     }
     public void PlayGame()
     {
-        Write("\nEnter your name: ");
-        string? playerName = ReadLine();
-        WriteLine($"Welcome to the Mathgame, {playerName}!\n");
-
-        Write("Choose an operator for the game between: \nA - '+' \nS - '-' \nM - 'x' \nD - ':' \ne - exit game\n");
-        WriteLine();
-        string? Input_game = ReadLine();
-        switch (Input_game)
+        try 
         {
-            case "A" or "a":
-                WriteLine("\nYou chose Addition!");
-                Addition addition = new Addition(_gState); // Passo l'istanza di RegToChrono al costruttore di Addition
-                addition.StartAdditionGame();
-                return; // Esco dal metodo per tornare al menu principale del gioco senza perdere lo stato del gioco
-            /*case "S" or "s":
-                WriteLine("You chose Subtraction!");
-                Subtraction subtraction = new Subtraction();
-                subtraction.StartSubtractionGame(playerName);
-                break;
-            case "M" or "m":
-                WriteLine("You chose Multiplication!");
-                Multiplication multiplication = new Multiplication();
-                multiplication.StartMultiplicationGame(playerName);
-                break;
-            case "D" or "d":
-                WriteLine("You chose Division!");
-                Division division = new Division();
-                division.StartDivisionGame(playerName);
-                break;*/
-            case "e":
-                WriteLine("\nExiting the game. Goodbye!\n");
-                return;
-            default:
-                WriteLine("Invalid choice. Please select a valid operator.");
-                break;
+            Write("\nEnter your name: ");
+            string? playerName = ReadLine();
+            if (string.IsNullOrWhiteSpace(playerName))
+            {
+                throw new ArgumentNullException("Name cannot be null.");
+            }
+            WriteLine($"Welcome to the Mathgame, {playerName}!\n");
+
+            Write("Choose an operator for the game between: \nA - '+' \nS - '-' \nM - 'x' \nD - ':' \ne - exit game\n");
+            WriteLine();
+            string? Input_game = ReadLine();
+            switch (Input_game)
+            {
+                case "A" or "a":
+                    WriteLine("\nYou chose Addition!");
+                    Addition addition = new Addition(_gState); // Passo l'istanza di RegToChrono al costruttore di Addition
+                    addition.StartAdditionGame();
+                    return; // Esco dal metodo per tornare al menu principale del gioco senza perdere lo stato del gioco
+                /*case "S" or "s":
+                    WriteLine("You chose Subtraction!");
+                    Subtraction subtraction = new Subtraction();
+                    subtraction.StartSubtractionGame(playerName);
+                    break;
+                case "M" or "m":
+                    WriteLine("You chose Multiplication!");
+                    Multiplication multiplication = new Multiplication();
+                    multiplication.StartMultiplicationGame(playerName);
+                    break;
+                case "D" or "d":
+                    WriteLine("You chose Division!");
+                    Division division = new Division();
+                    division.StartDivisionGame(playerName);
+                    break;*/
+                case "e":
+                    WriteLine("\nExiting the game. Goodbye!\n");
+                    return;
+                default:
+                    WriteLine("Invalid choice. Please select a valid operator.");
+                    break;
+            }
+        }
+        catch (ArgumentNullException ex)
+        {
+            WriteLine($"\nERROR!: {ex.Message}\n");
         }
     }
 
