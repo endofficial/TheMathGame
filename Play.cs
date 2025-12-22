d7252bf9c88282b26ab4c4854d024945d33df5c7
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2025-12-22 23:29:49 +0100

    #I removed try-catch block

--- Play.cs
+++ Play.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+
 public class Play 
 {
     // Composizione: Play "ha un" RegToChrono per registrare le partite giocate
@@ -7,84 +8,72 @@ public class Play
     {
         _gState = gState;
     }
+
     public void PlayGame()
     {
         bool isPlaying = true;
         while (isPlaying)
         {
-            try
+            Write("\nEnter your name: ");
+            string? playerName = ReadLine();
+            if (string.IsNullOrWhiteSpace(playerName))
             {
-                Write("\nEnter your name: ");
-                string? playerName = ReadLine();
-                if (string.IsNullOrWhiteSpace(playerName)) // Controllo se il nome è null o vuoto
-                {
-                    throw new ArgumentNullException("Name cannot be null.");
-                }
+                WriteLine("\nERROR!: Name cannot be null or empty.\n");
+                continue;
+            }
+
+            else
+            {
                 WriteLine($"Welcome to the Mathgame, {playerName}!\n");
                 _gState.GetPlayerName(playerName); // Aggiungo il nome del giocatore alla lista
-
-            }
-            catch (ArgumentNullException ex)
-            {
-                WriteLine($"\nERROR!: {ex.Message}\n");
-                continue; // Torno all'inizio del ciclo while per chiedere nuovamente il nome
             }
 
             while (isPlaying)
             {
-                try
+                Write("\nChoose an operator for the game between: \nA -> '+' \nS -> '-' \nM -> 'x' \nD -> ':' \ne -> exit game\n");
+                string? Input_game = ReadLine();
+                if (string.IsNullOrWhiteSpace(Input_game))
                 {
-                    Write("Choose an operator for the game between: \nA -> '+' \nS -> '-' \nM -> 'x' \nD -> ':' \ne -> exit game\n");
-                    string? Input_game = ReadLine();
-                    if (string.IsNullOrWhiteSpace(Input_game))
-                    {
-                        throw new ArgumentNullException("Input cannot be null or empty.");
-                    }
-                    char inputChar = Input_game[0]; // Prendo il primo carattere dell'input
-                    char upperChar = char.ToUpper(inputChar);
-                    if (!(upperChar == 'A' || upperChar == 'S' || upperChar == 'M' || upperChar == 'D' || upperChar == 'E'))
-                    {
-                        throw new ArgumentException("Invalid input. Please enter 'A' for addition, 'S' for subtraction, 'M' for multiplication, 'D' for division, or 'e' to exit.");
-                    }
-                    switch (Input_game)
-                    {
-                        case "A" or "a":
-                            WriteLine("\nYou chose Addition!");
-                            Addition addition = new Addition(_gState); // Passo l'istanza di RegToChrono al costruttore di Addition
-                            addition.StartAdditionGame();
-                            return; // Esco dal metodo per tornare al menu principale del gioco senza perdere lo stato del gioco
-                        case "S" or "s":
-                            WriteLine("\nYou chose Subtraction!");
-                            Subtraction subtraction = new Subtraction(_gState);
-                            subtraction.StartSubtractionGame();
-                            return;
-                        case "M" or "m":
-                            WriteLine("\nYou chose Multiplication!");
-                            Multiplication multiplication = new Multiplication(_gState);
-                            multiplication.StartMultiplicationGame();
-                            return;
-                        case "D" or "d":
-                            WriteLine("\nYou chose Division!");
-                            Division division = new Division(_gState);
-                            division.StartDivisionGame();
-                            return;
-                        case "e":
-                            WriteLine("\nExiting the game. Goodbye!\n");
-                            return;
-                        default:
-                            WriteLine("Invalid choice. Please select a valid operator.");
-                            break;
-                    }
-                }
-                catch (ArgumentNullException ex)
-                {
-                    WriteLine($"\nERROR!: {ex.Message}\n");
+                    WriteLine("Input cannot be null or empty.");
                     continue;
                 }
-                catch (ArgumentException ex)
+
+                char inputChar = Input_game[0]; // Prendo il primo carattere dell'input
+                char upperChar = char.ToUpper(inputChar);
+                if (!(upperChar == 'A' || upperChar == 'S' || upperChar == 'M' || upperChar == 'D' || upperChar == 'E'))
                 {
-                    WriteLine($"\nERROR!: {ex.Message}\n");
-                    continue;
+                   WriteLine("Invalid input. Please enter 'A' for addition, 'S' for subtraction, 'M' for multiplication, 'D' for division, or 'e' to exit.");
+                   continue;
+                }
+
+                switch (Input_game)
+                {
+                    case "A" or "a":
+                        WriteLine("\nYou chose Addition!");
+                        Addition addition = new Addition(_gState); // Passo l'istanza di RegToChrono al costruttore di Addition
+                        addition.StartAdditionGame();
+                        return; // Esco dal metodo per tornare al menu principale del gioco senza perdere lo stato del gioco
+                    case "S" or "s":
+                        WriteLine("\nYou chose Subtraction!");
+                        Subtraction subtraction = new Subtraction(_gState);
+                        subtraction.StartSubtractionGame();
+                        return;
+                    case "M" or "m":
+                        WriteLine("\nYou chose Multiplication!");
+                        Multiplication multiplication = new Multiplication(_gState);
+                        multiplication.StartMultiplicationGame();
+                        return;
+                    case "D" or "d":
+                        WriteLine("\nYou chose Division!");
+                        Division division = new Division(_gState);
+                        division.StartDivisionGame();
+                        return;
+                    case "e":
+                        WriteLine("\nExiting the game. Goodbye!\n");
+                        return;
+                    default:
+                        WriteLine("Invalid choice. Please select a valid operator.");
+                        break;
                 }
             }
         }     
